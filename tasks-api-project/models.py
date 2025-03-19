c8db20e6557649c407e5dbf69e0aafd86a0300a3
Adrian Paredes <adrianparedesroman99@gmail.com>
2025-03-19 10:34:04 -0400

    database creation

--- tasks-api-project/models.py
+++ tasks-api-project/models.py
@@ -1,12 +1,22 @@
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import create_engine
+from typing import Optional
+from sqlalchemy.orm import DeclarativeBase, mapped_column
+from sqlalchemy import Integer, create_engine, String
 
 DATABASE_URI = "postgres://postgres:password@localhost/taskmanagement"
 
-engine = create_engine()
+# engine is a global object that establishes the connection to our db
+engine = create_engine(DATABASE_URI, echo=False) 
 
 class Base(DeclarativeBase):
     pass
 
 class Tasks(Base):
-    pass
+    __tablename__ = "tasks"
+    # since we are using declararative mapping to create this table, we can use the mapped_column method to have
+    # to have additional metadata about the table, but it serves the same purpose as using Column.
+    id = mapped_column(Integer, primary_key=True, nullable=False)
+    title = mapped_column(String(30), nullable=False)
+    description = mapped_column(String, nullable=False)
+    status = mapped_column(String(10), nullable=False)
+    priority: Optional[str] = mapped_column()
+    due_date: Optional[str] = mapped_column()
