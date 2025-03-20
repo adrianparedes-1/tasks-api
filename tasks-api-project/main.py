b9ba37063b110f15a4571ba5d8c9e9cd641996b2
Adrian Paredes <adrianparedesroman99@gmail.com>
2025-03-20 15:01:30 -0400

    completed crud

--- tasks-api-project/main.py
+++ tasks-api-project/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, status
+from fastapi import FastAPI, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from . import database, models, schemas
 
@@ -23,11 +23,49 @@ def get_tasks(db: Session = Depends(database.get_db)):
 @app.post("/tasks/", status_code=status.HTTP_201_CREATED)
 def create_task(task: schemas.Task, db: Session = Depends(database.get_db)):
     # turn input into dict
-    print("hello")
-    task_dict = models.Tasks(**task.model_dump())
-    # task_dict = schemas.Task.model_dump(**task)
-    print(task_dict)
-    db.add(task_dict)
-    db.flush()
+    new_task = models.Tasks(**task.model_dump())
+    db.add(new_task)
+    db.commit()
     
-    return task_dict
+    return new_task
+
+
+@app.get("/tasks/{id}")
+def get_task(id: int, db: Session = Depends(database.get_db)):
+    #query all and select first instance
+    task = db.query(models.Tasks).filter(models.Tasks.id == id).first()
+    #handle error if id is not found
+    if not task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Task with id: {id} was not found.")
+    
+    #return task with matching id
+    return task
+
+@app.delete("/tasks/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(id: int, db: Session = Depends(database.get_db)):
+    # find task with matching id
+    task = db.query(models.Tasks).filter(models.Tasks.id == id).first()
+    print(task)
+    # validate it exists and raise appropriate error
+    if not task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Task with id: {id} was not found.")
+    # delete from db
+    db.delete(task)
+    # commit to db
+    db.commit()
+
+
+@app.put("/tasks/{id}")
+def update_task(id: int, task: schemas.Task, db: Session = Depends(database.get_db)):
+    #find task with with matching id and validate if it is found
+    task_query = db.query(models.Tasks).filter(models.Tasks.id == id)
+    # print(task_query)
+    if not task_query.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Task with id: {id} was not found.")
+    # updated_task = models.Tasks(**task.model_dump())
+    # the statement above is equivalent to doing models.Tasks(task.title=title, task.description=description, etc.)
+    # the staement below just creates a dictionary from the task object
+    updated_task = task.model_dump()
+    task_query.update(updated_task, synchronize_session=False)
+    db.commit()
+    return updated_task
--- tasks-api-project/models.py
+++ tasks-api-project/models.py
@@ -9,9 +9,9 @@ class Tasks(Base):
     # since we are using declararative mapping to create this table, we can use the mapped_column method to have
     # to have additional metadata about the table, but it serves the same purpose as using Column.
     id = mapped_column(Integer, primary_key=True, nullable=False)
-    title = mapped_column(String(30), nullable=False)
+    title = mapped_column(String, nullable=False)
     description = mapped_column(String, nullable=False)
-    status = mapped_column(String(10), nullable=False)
+    status = mapped_column(String, nullable=False)
     priority: Mapped[Optional[str]] = mapped_column()
     due_date: Mapped[Optional[str]] = mapped_column()
     
