a3ae7b4f161fc4f808a64a02ab2bb4abbef3414e
Adrian Paredes <adrianparedesroman99@gmail.com>
2025-03-20 12:39:28 -0400

    crud operations updated

--- tasks-api-project/main.py
+++ tasks-api-project/main.py
@@ -1,10 +1,33 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
 from sqlalchemy.orm import Session
-from . import database, models
+from . import database, models, schemas
 
 app = FastAPI()
 
 
-# @app.get("/test")
-# def testing(db: Session = Depends(database.get_db())):
-#     pass
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/test")
+def testing(db: Session = Depends(database.get_db)):
+    return {"test": "success"}
+
+
+@app.get("/tasks")
+def get_tasks(db: Session = Depends(database.get_db)):
+    tasks = db.query(models.Tasks).all()
+    return tasks
+
+
+@app.post("/tasks/", status_code=status.HTTP_201_CREATED)
+def create_task(task: schemas.Task, db: Session = Depends(database.get_db)):
+    # turn input into dict
+    print("hello")
+    task_dict = models.Tasks(**task.model_dump())
+    # task_dict = schemas.Task.model_dump(**task)
+    print(task_dict)
+    db.add(task_dict)
+    db.flush()
+    
+    return task_dict
--- tasks-api-project/schemas.py
+++ tasks-api-project/schemas.py
@@ -3,7 +3,6 @@ from typing import Optional
 from pydantic import BaseModel
 
 class Task(BaseModel):
-    id: int
     title: str
     description: str
     status: str
